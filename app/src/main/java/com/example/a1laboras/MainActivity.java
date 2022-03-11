64ac503c465af028a40192b7ced45086e446fb11
AgneRdz <agne.radz@gmail.com>
2022-03-11 19:43:13 +0200

    Added Main button

--- app/src/main/java/com/example/a1laboras/MainActivity.java
+++ app/src/main/java/com/example/a1laboras/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.a1laboras;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnMainClick(View view) {
+        TextView tvMain = (TextView)findViewById(R.id.tvMain);
+        tvMain.setText("smth new");
     }
 }
